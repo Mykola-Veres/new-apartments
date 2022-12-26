d604a48ac4749e9a0aaeabf3d1a368537fd6f386
Mykola-Veres <veresnickolay@gmail.com>
2022-12-26 17:24:58 +0200

    add-reset

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,49 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <img alt="Vue logo" src="./assets/logo.png" />
   <NewCopponentVue v-on:click="increment" :id="$style.app"></NewCopponentVue>
-  <MainButton @click="increment" type="button" outline> <span> By</span> Click Me </MainButton>
+  <MainButton @click="increment" type="button" outline>
+    <span> By</span> Click Me
+  </MainButton>
   <h2 :id="$style.app">{{ title }}</h2>
   <DerectiveTestVue></DerectiveTestVue>
   <StarRatingVue :rating="3.6"></StarRatingVue>
 </template>
 
 <script>
-
-import MainButton from './components/MainButton.vue';
-import NewCopponentVue from './components/NewCopponent.vue';
-import DerectiveTestVue from './components/DerectiveTest.vue';
-import StarRatingVue from './components/StarRating.vue';
+import MainButton from "./components/MainButton.vue";
+import NewCopponentVue from "./components/NewCopponent.vue";
+import DerectiveTestVue from "./components/DerectiveTest.vue";
+import StarRatingVue from "./components/StarRating.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     NewCopponentVue,
     MainButton,
     DerectiveTestVue,
     StarRatingVue,
-},
-data(){return{
-  amountOfClicks: 10
-}},
-computed: {
-  title(){
-    return`Amount of clicks ${this.amountOfClicks}`}
-},
-methods: {
-  increment(){
-    this.amountOfClicks += 10
-  }
-}
-}
+  },
+  data() {
+    return {
+      amountOfClicks: 10,
+    };
+  },
+  computed: {
+    title() {
+      return `Amount of clicks ${this.amountOfClicks}`;
+    },
+  },
+  methods: {
+    increment() {
+      this.amountOfClicks += 10;
+    },
+  },
+};
 </script>
 
 <style module>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Montserrat, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
