9400bcb6261d90e9144127ef40f1cb388e862e9d
Mykola-Veres <veresnickolay@gmail.com>
2022-12-30 22:11:03 +0200

    add

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
-import { createApp } from "vue";
+import { createApp, Vue } from "vue";
 import App from "./App.vue";
 import "./assets/scss/index.scss";
+import VueRouter from "vue-router";
+
+Vue.use(VueRouter);
 
 createApp(App).mount("#app");
